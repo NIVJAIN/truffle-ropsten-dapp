866ae8301c81dadcfadf5eccadaa20bfda3a35e2
NIVJAIN <sripal.jain@gmail.com>
2022-02-21 10:05:05 +0800

    ropsten2ganacheswitch

--- middlewares/provider.js
+++ middlewares/provider.js
@@ -1,13 +1,25 @@
 const Web3 = require('web3')
-
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+const MNEMONIC = 'keen beef way never plunge like slab shove unhappy file same rely'
+const ROPSTEN_URL = "https://ropsten.infura.io/v3/49ddda8aa6f44ea9a479729898a4024f"
+const GANACHE_URL = 'http://localhost:7545'
+const ETHEREUM_NETWORK_SELECTION = process.env.ETHEREUM_NETWORK_SELECTION || "GANACHE"
 class  Provider {
   constructor() {
     //setup web3 provider
     try {
-      this.web3 = new Web3(
-        // new Web3.providers.HttpProvider('http://localhost:7545'),
-        new Web3.providers.HttpProvider(process.env.BLOCKCHAIN_URL)
-      )
+      switch (ETHEREUM_NETWORK_SELECTION) {
+        case "ROPSTEN":
+          console.log("PROVIDER CHOOSEN ", ETHEREUM_NETWORK_SELECTION )
+          this.web3 = new Web3(new HDWalletProvider(MNEMONIC,ROPSTEN_URL))
+          break;
+        case "GANACHE":
+          this.web3 = new Web3.providers.HttpProvider(GANACHE_URL)
+          break;
+        default:
+          this.web3 = new Web3.providers.HttpProvider(GANACHE_URL)
+          break;
+      }
     }catch (error){
       console.log("Middleware-Provider",error)
     }
